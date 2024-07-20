5ae1a5e2976c59594c3cc31ccb31c5547ea750af
austintyrome <austintyrome@gmail.com>
2024-07-20 15:30:53 -0600

    Added Resume

--- src/components/PortfolioContainer.jsx
+++ src/components/PortfolioContainer.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react';
 import NavTabs from './NavTabs';
-import Home from './pages/Home';
 import About from './pages/About';
 import Project from './pages/Project';
 import Contact from './pages/Contact';
+import Resume from './pages/Resume';
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('Home');
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
-    if (currentPage === 'Home') {
-      return <Home />;
+    if (currentPage === 'Resume') {
+      return <Resume />;
     }
     if (currentPage === 'About') {
       return <About />;
