637d52ea294c75358c4346116b1a7ae5581acb90
austintyrome <austintyrome@gmail.com>
2024-07-24 09:42:52 -0600

    Added Resume and Project Card

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -1,9 +1,71 @@
 export default function Resume() {
     return (
       <div>
-        <h1>Resume Page</h1>
+   <h1>Download My Resume</h1>
+      <a href="/resume.pdf" download>
+        <button>Download Resume</button>
+      </a>
+
+        <h2>Resume</h2>
         <p>
-       insert Resume
+        Austin Godfrey
+(620) 640-2007
+AustinTyrome@gmail.com
+
+Education
+
+Web Development Program August 2024
+University of Denver
+Medical Assistant Program December 2020
+Aims Community College- Greeley, CO
+Nursing Assistant Program Summer 2018
+Aims Community College– Greeley, CO
+Phlebotomy Program Spring 2019
+Aims Community College– Greeley, CO
+
+Certifications
+
+Certified Medical Assistant March 2021
+National Center for Competency Training
+Basic Life Support Certified February 2020
+American Heart Association
+
+Work Experience
+
+Medical Assistant March 2024 - Current
+
+Medical Assistant December 2021 – 2024
+CU Family Medicine Centennial
+• Checked in patients, checked vitals, draws Labs.
+• Assist in completing medical records, efficient in most charting systems.
+• Order and keep inventory of all supplies.
+• Train all incoming staff regarding policies and procedures of clinic.
+ Able to fax out information when appropriate
+ Able to scan in patient information and input into charts
+ Help establish VFC vaccines at clinic
+ Manage Tricare prior authorizations
+ Able to work with pediatric patients and train others to work with them
+ Able to train new staff and staff that float to office
+
+Medical Assistant November 2020- December 2021
+Miramont Wellness Center
+• Lead Medial Assistant on the provider’s team.
+• Checked in patients, checked vitals, draws Labs.
+• Assisted provider with biopsies.
+• Able to complete EKG’s.
+• Assist in completing medical records, efficient in most charting systems.
+• Follow up with patients, take phone calls.
+• Able to communicate effectively with team members and patients
+ Co-ran diabetic clinic with provider (pump downloads and insulin adjustments)
+ Helped manage provider’s schedule
+Manager March 2016-December 2021
+Dairy Queen
+• Supervisor of the team.
+• Worked to manage any crisis, solve customer complaints.
+• Completed monetary transactions, bookkeeping.
+
+• Make sure the shift ran efficiently and smoothly.
+ Organize and monitor large quantities of supplies
         </p>
       </div>
     );
