66a910b02f6fd6ec621b923c5bd4601cff298bd5
austintyrome <austintyrome@gmail.com>
2024-08-06 15:08:11 -0600

    added index.js file to components

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Header } from './Header';
+export { default as Footer } from './Footer';
+export { default as AboutMe } from './AboutMe';
+export { default as Contact } from './Contact';
+export { default as Portfolio } from './Portfolio';
+export { default as Resume } from './Resume';
