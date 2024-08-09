f7a3b1c4634ac9a8cc4e65fab0d3e5a266b4f004
austintyrome <austintyrome@gmail.com>
2024-08-08 18:42:34 -0600

    finished aboutMe and added linkedin

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,31 +5,10 @@ function AboutMe() {
       <div className='card'>
           <div>
               <div>
-              <img src='' alt='Profile pic'></img>
+              <img src="../../public/IMG_5684-merged.pdf" alt='Profile pic'></img>
               <h2>About Me</h2>
               </div>
-             <p>My name is Austin Godfrey. I am looking to make a career change into web development. I have worked in the medical field for the last 4 years medical assisting and before that I was a manger at Dairy Queen. A cornerstone of who I am is my friendliness and approachability. I believe in creating a welcoming environment where colleagues and clients feel valued and understood. My ability to build rapport quickly has helped me establish strong professional relationships, enhance team cohesion, and improve client satisfaction. My leadership style is collaborative and inclusive, empowering team members to contribute their best while fostering a culture of mutual respect and shared success. Problem-solving is one of my strongest attributes. I approach challenges with a solution-oriented mindset. 
-
-Management
-My journey in management has equipped me with the ability to lead teams effectively, ensuring projects are completed on time and within budget. I excel at coordinating resources, setting clear goals, and maintaining a focused approach to achieve organizational objectives. My leadership style is collaborative and inclusive, empowering team members to contribute their best while fostering a culture of mutual respect and shared success.
-
-Friendliness
-A cornerstone of my professional ethos is my friendliness and approachability. I believe in creating a welcoming environment where colleagues and clients feel valued and understood. My ability to build rapport quickly has helped me establish strong professional relationships, enhance team cohesion, and improve client satisfaction. This friendly demeanor extends beyond the workplace, making me a trusted and reliable figure in both professional and personal circles.
-
-Multitasking
-In today's fast-paced work environment, the ability to multitask is crucial. I have honed this skill through years of managing multiple projects simultaneously, ensuring that each task receives the attention it deserves. My organizational skills and strategic planning enable me to juggle various responsibilities without compromising on quality or efficiency. Whether it's balancing daily operations with long-term projects or addressing urgent issues while maintaining routine tasks, I handle it all with precision and poise.
-
-Problem Solving
-Problem-solving is one of my strongest attributes. I approach challenges with a solution-oriented mindset, breaking down complex issues into manageable parts and exploring innovative ways to overcome obstacles. My analytical skills and creativity allow me to identify root causes and implement effective solutions, turning potential setbacks into opportunities for growth and improvement. This proactive approach has consistently led to enhanced performance and successful outcomes in every role I've undertaken.
-
-Adaptability
-The ability to adapt to changing circumstances is essential in any professional setting. I pride myself on my flexibility and resilience, readily adjusting to new situations, technologies, and environments. This adaptability has enabled me to thrive in diverse roles and industries, quickly learning new skills and applying them effectively. My openness to change and continuous learning ensures that I remain at the forefront of industry developments, ready to tackle any challenge that comes my way.
-
-Strong Work Ethic
-A strong work ethic underpins all my professional endeavors. I am committed to delivering high-quality work, consistently meeting or exceeding expectations. My dedication and perseverance drive me to put in the necessary effort to achieve my goals, no matter the obstacles. This relentless pursuit of excellence has earned me a reputation as a reliable and dependable professional, trusted to deliver results time and again.
-
-Communication
-Effective communication is the glue that binds all my skills together. Whether it's conveying complex ideas clearly, listening actively to understand others, or providing constructive feedback, my communication skills are second to none. I excel at tailoring my message to suit different audiences, ensuring clarity and engagement. This ability to communicate effectively has been instrumental in building strong teams, fostering collaboration, and maintaining positive client relationships.</p>
+             <p>My name is Austin Godfrey. I am looking to make a career change into web development. I have worked in the medical field for the last 4 years medical assisting and before that I was a manger at Dairy Queen. A cornerstone of who I am is my friendliness and approachability. I believe in creating a welcoming environment where colleagues and clients feel valued and understood. My ability to build rapport quickly has helped me establish strong professional relationships, enhance team cohesion, and improve client satisfaction. My leadership style is collaborative and inclusive, empowering team members to contribute their best while fostering a culture of mutual respect and shared success. Problem-solving is one of my strongest attributes. I approach challenges with a solution-oriented mindset. In today's fast-paced work environment, the ability to multitask is crucial. I have honed this skill through years of managing multiple projects simultaneously, ensuring that each task receives the attention it deserves. The ability to adapt to changing circumstances is essential in any professional setting. I pride myself on my flexibility and resilience, readily adjusting to new situations, technologies, and environments. A strong work ethic underpins all my professional endeavors. I am committed to delivering high-quality work, consistently meeting or exceeding expectations. Effective communication is the glue that binds all my skills together. Whether it's conveying complex ideas clearly, listening actively to understand others, or providing constructive feedback, my communication skills are second to none.</p>
               
           </div>
       </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer>
       <a href="https://github.com/austintyrome?tab=repositories" target="_blank" rel="">GitHub</a>
-      <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      <a href="www.linkedin.com/in/austin-godfrey-2b3354269" target="_blank" rel="noopener noreferrer">LinkedIn</a>
       <a href="https://stackoverflow.com/users/yourprofile" target="_blank" rel="noopener noreferrer">Stack Overflow</a>
     </footer>
   );
