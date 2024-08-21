a3bc6c805c84af1634a0682f28d004fe575616a7
austintyrome <austintyrome@gmail.com>
2024-08-21 16:14:55 -0600

    omitted footer

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Nav } from './nav';
-export { default as Footer } from './footer';
+// export { default as Footer } from './footer';
 export { default as AboutMe } from './aboutMe';
 export { default as Contact } from './contact';
 export { default as Portfolio } from './portfolio';
